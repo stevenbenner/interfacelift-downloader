5f42b292fa7fcf05ffe9a450edd39a7506be62d9
Steven Benner <admin@stevenbenner.com>
2014-04-27 14:18:14 -0700

    Added Grunt process to automatically lint project.

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+/*
+ * Grunt Config
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // configure grunt
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      grunt: {
+        src: [ 'Gruntfile.js' ],
+        options: {
+          jshintrc: '.jshintrc'
+        }
+      },
+      bin: {
+        src: [ 'bin/*' ],
+        options: {
+          jshintrc: '.jshintrc'
+        }
+      },
+      lib: {
+        src: [ 'lib/*.js' ],
+        options: {
+          jshintrc: '.jshintrc'
+        }
+      }
+    },
+    jsonlint: {
+      lib: {
+        src: [ 'lib/*.json' ]
+      },
+      project: {
+        src: [ '.jshintrc', 'package.json' ]
+      }
+    }
+  });
+
+  // load grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jsonlint');
+
+  // register grunt tasks
+  grunt.registerTask('default', [ 'jshint', 'jsonlint' ]);
+
+};
