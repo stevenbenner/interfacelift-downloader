c741ed8b4aeeefaa564bfd597d3c98229453992c
Steven Benner <admin@stevenbenner.com>
2017-01-05 08:11:11 -0800

    Added JSCS validation to the Grunt testing process

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,21 +28,36 @@ module.exports = function(grunt) {
         }
       }
     },
+    jscs: {
+      options: {
+        config: '.jscsrc'
+      },
+      grunt: {
+        src: [ 'Gruntfile.js' ]
+      },
+      bin: {
+        src: [ 'bin/*' ]
+      },
+      lib: {
+        src: [ 'lib/*.js' ]
+      }
+    },
     jsonlint: {
       lib: {
         src: [ 'lib/*.json' ]
       },
       project: {
-        src: [ '.jshintrc', 'package.json' ]
+        src: [ '.jscsrc', '.jshintrc', 'package.json' ]
       }
     }
   });
 
   // load grunt plugins
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
   grunt.loadNpmTasks('grunt-jsonlint');
 
   // register grunt tasks
-  grunt.registerTask('default', [ 'jshint', 'jsonlint' ]);
-  grunt.registerTask('travis', [ 'jshint', 'jsonlint' ]);
+  grunt.registerTask('default', [ 'jshint', 'jscs', 'jsonlint' ]);
+  grunt.registerTask('travis', [ 'jshint', 'jscs', 'jsonlint' ]);
 };
